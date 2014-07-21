0d6c1e731ff25db3603b75b51992b8208407fb0a
Ilya Kantor <iliakan@gmail.com>
2014-07-21 10:55:21 +0400

    make more flexible

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Flexible logging wrapper around winston
  *
  * usage:
- *  var log = require('lib/log')(module)
+ *  var log = require('lib/log')()
  *  log.debug("Winston %s", "syntax");
  *
  * enabling debug directly in the code:
